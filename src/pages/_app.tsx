b79d383fa146aae81cc00f940c0ee74dcabc654b
Carlos Romero <cromerosi@unal.edu.co>
2026-03-19 23:22:46 -0500

    Quitandome responsabilidad de encima y dandosela a jose

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </div>
       <footer className="border-t border-white/10 bg-slate-950/85 px-6 py-4 text-center text-xs tracking-[0.08em] text-slate-200/90 backdrop-blur">
         Creado por: Carlos Romero · cromerosi@unal.edu.co | Jonathan Ramírez · jonramirez@unal.edu.co
+        Ayuda, soporte y preguntas: Jose	Fernando Lopez Ramirez · jlopezra@unal.edu.co
       </footer>
     </div>
   );
